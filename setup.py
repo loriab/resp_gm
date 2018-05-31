6d43cb6c7da5a5e4781ee5272a8f95239607aebb
Lori A. Burns <lori.burns@gmail.com>
2018-05-31 13:35:59 -0400

    setup travis

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='resp',
+    url='https://github.com/alenaizan/resp',
+    license='BSD license',
+    packages=['resp'],
+    zip_safe=False,
+    classifiers=[
+          "Intended Audience :: Developers",
+          "Operating System :: OS Independent",
+          "Topic :: Software Development",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Development Status :: 5 - Production/Stable",
+          "License :: OSI Approved :: BSD License"
+    ],
+)
